b0d4cf78bd99cc5ea80c1dc607ca8570b15c57c9
Aleksand Muzhev <muzhev1990@mail.ru>
2023-06-02 18:26:55 +0500

    Add Tests in BankTest Second

--- src/test/java/ru/netology/javaqadiplom/BankTest.java
+++ src/test/java/ru/netology/javaqadiplom/BankTest.java
@@ -104,5 +104,134 @@ class BankTest {
         Assertions.assertEquals(9_000, accountFrom.getBalance());
         Assertions.assertEquals(-4_000, accountTo.getBalance());
     }
+
+    @Test
+    public void testAmountLowFromBalance() {
+        Bank bank = new Bank();
+        Account accountFrom = new Account(800);
+        Account accountTo = new Account(200);
+
+        bank.transfer(accountFrom, accountTo, 799);
+
+        Assertions.assertEquals(1, accountFrom.getBalance());
+        Assertions.assertEquals(999, accountTo.getBalance());
+    }
+
+    @Test
+    public void testAmountCurrentFromBalance() {
+        Bank bank = new Bank();
+        Account accountFrom = new Account(800);
+        Account accountTo = new Account(200);
+
+        bank.transfer(accountFrom, accountTo, 800);
+
+        Assertions.assertEquals(0, accountFrom.getBalance());
+        Assertions.assertEquals(1000, accountTo.getBalance());
+    }
+
+    @Test
+    public void testAmountHighFromBalance() {
+        Bank bank = new Bank();
+        Account accountFrom = new Account(800);
+        Account accountTo = new Account(200);
+
+        bank.transfer(accountFrom, accountTo, 801);
+
+        Assertions.assertFalse(false);
+        Assertions.assertEquals(800, accountFrom.getBalance());
+        Assertions.assertEquals(200, accountTo.getBalance());
+    }
+
+    @Test
+    public void testAmountNegative() {
+        Bank bank = new Bank();
+        Account accountFrom = new Account(800);
+        Account accountTo = new Account(200);
+
+        bank.transfer(accountFrom, accountTo, -500);
+
+        Assertions.assertFalse(false);
+        Assertions.assertEquals(800, accountFrom.getBalance());
+        Assertions.assertEquals(200, accountTo.getBalance());
+    }
+
+    @Test
+    public void testSuccessAmount() {
+        Bank bank = new Bank();
+        Account accountFrom = new Account(800);
+        Account accountTo = new Account(200);
+
+        bank.transfer(accountFrom, accountTo, 400);
+
+        Assertions.assertTrue(true);
+        Assertions.assertEquals(400, accountFrom.getBalance());
+        Assertions.assertEquals(600, accountTo.getBalance());
+    }
+
+    @Test
+    public void testSuccessWhenFromLowTo() {
+        Bank bank = new Bank();
+        Account accountFrom = new Account(400);
+        Account accountTo = new Account(800);
+
+        bank.transfer(accountFrom, accountTo, 200);
+
+        Assertions.assertTrue(true);
+        Assertions.assertEquals(200, accountFrom.getBalance());
+        Assertions.assertEquals(1000, accountTo.getBalance());
+    }
+
+    @Test
+    public void testFailedWhenFromNull() {
+        Bank bank = new Bank();
+        Account accountFrom = new Account(0);
+        Account accountTo = new Account(400);
+
+        bank.transfer(accountFrom, accountTo, 200);
+
+        Assertions.assertFalse(false);
+        Assertions.assertEquals(0, accountFrom.getBalance());
+        Assertions.assertEquals(400, accountTo.getBalance());
+    }
+
+    @Test
+    public void testFailedWhenToNull() {
+        Bank bank = new Bank();
+        Account accountFrom = new Account(800);
+        Account accountTo = new Account(0);
+
+        bank.transfer(accountFrom, accountTo, 200);
+
+        Assertions.assertTrue(true);
+        Assertions.assertEquals(600, accountFrom.getBalance());
+        Assertions.assertEquals(200, accountTo.getBalance());
+    }
+
+    @Test
+    public void testFailedWhenFromNegative() {
+        Bank bank = new Bank();
+        Account accountFrom = new Account(-200);
+        Account accountTo = new Account(400);
+
+        bank.transfer(accountFrom, accountTo, 200);
+
+        Assertions.assertFalse(false);
+        Assertions.assertEquals(-200, accountFrom.getBalance());
+        Assertions.assertEquals(400, accountTo.getBalance());
+    }
+
+    @Test
+    public void testSuccessWhenToNegative() {
+        Bank bank = new Bank();
+        Account accountFrom = new Account(800);
+        Account accountTo = new Account(-200);
+
+        bank.transfer(accountFrom, accountTo, 400);
+
+        Assertions.assertTrue(true);
+        Assertions.assertEquals(400, accountFrom.getBalance());
+        Assertions.assertEquals(200, accountTo.getBalance());
+    }
+
 }
 
